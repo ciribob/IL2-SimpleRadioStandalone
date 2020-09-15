285d72aa6f27aaa1825889c49fb089205f99a6eb
Mike Mason <mike@mikemason.ca>
2020-09-15 14:11:43 -0600

    New log/server monitoring, start of server-side coalition security enforcement.

---
+++ IL2-SimpleRadio Server/Network/DserverCoalitionSecurityChecker.cs
@@ -0,0 +1,256 @@
+﻿using Ciribob.IL2.SimpleRadio.Standalone.Common.Network;
+using Ciribob.IL2.SimpleRadio.Standalone.Common.Setting;
+using Ciribob.IL2.SimpleRadio.Standalone.Server.Settings;
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace Ciribob.IL2.SimpleRadio.Standalone.Server.Network
+{
+    /// <summary>
+	/// Check a client list against a running dserver, to ensure no-one is cheating
+    /// by tuning into a coalition they're not allowed to. This requires both rcon
+    /// access and logfile access.
+    /// 
+    /// If a user is flying as Axis, they can be Axis or Neutral.
+    /// If a user is flying as Allied, they can be Allied or Neutral.
+    /// </summary>
+    public class DserverCoalitionSecurityChecker
+    {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        private Socket socket;
+        private string socketAddress = string.Empty;
+        private string rconUser = string.Empty;
+        private string rconPassword = string.Empty;
+
+        public void PerformCheck(ICollection<SRClient> currentClients)
+        {
+            if (EnsureConnected())
+            {
+                // Do the check
+                string playerListResponse = SendCommand("getplayerlist");
+                if (GetResponseCode(playerListResponse) != 1)
+                {
+                    Logger.Warn("Could not retrieve current player list.");
+                    return;
+                }
+                List<PlayerInfo> players = DecodePlayerList(playerListResponse);
+                // TODO: Also spin up an IL2LogMonitor to determine what coalition players are spawned as.
+                //       For every player on SRS who is *not* in the neutral channel, check
+                //       - are they on the dserver player list? if not, mute / deafen them on SRS
+                //       - did they most recently spawn in a plane matching their SRS coalition? if not, mute / deafen them on SRS
+            }
+            else
+            {
+                Logger.Warn("Not connected to dserver, skipping coalition security check.");
+            }
+        }
+
+        private bool EnsureConnected()
+        {
+            string desiredAddress = ServerSettingsStore.Instance.GetGeneralSetting(ServerSettingsKeys.DSERVER_RCON_ADDRESS).StringValue;
+            string desiredUser = ServerSettingsStore.Instance.GetGeneralSetting(ServerSettingsKeys.DSERVER_RCON_USERNAME).StringValue;
+            string desiredPassword = ServerSettingsStore.Instance.GetGeneralSetting(ServerSettingsKeys.DSERVER_RCON_PASSWORD).StringValue;
+
+            if (socket == null)
+            {
+                return ConnectAndAuthorize(desiredAddress, desiredUser, desiredPassword);
+            }
+            else if (!socketAddress.Equals(desiredAddress) || !rconUser.Equals(desiredUser) || !rconPassword.Equals(desiredPassword))
+            {
+                ShutdownSocket();
+                return ConnectAndAuthorize(socketAddress, rconUser, rconPassword);
+            }
+            else
+            {
+                if (!EnsureAuthorized())
+                {
+                    ShutdownSocket();
+                    return ConnectAndAuthorize(socketAddress, rconUser, rconPassword);
+                }
+                return true;
+            }
+        }
+
+        private void ShutdownSocket()
+        {
+            socket.Shutdown(SocketShutdown.Both);
+            socket.Close();
+            socket = null;
+        }
+
+        private bool EnsureAuthorized()
+        {
+            return GetResponseCode(SendCommand("mystatus")) == 1;
+        }
+
+        private bool ConnectAndAuthorize(string address, string user, string password)
+        {
+            try
+            {
+                string[] addressAndPort = address.Split(':');
+                if (addressAndPort.Length != 2)
+                {
+                    Logger.Error("Invalid host address and port: {0}", addressAndPort);
+                    return false;
+                }
+                IPAddress iPAddress;
+                int port;
+                if (!IPAddress.TryParse(addressAndPort[0], out iPAddress))
+                {
+                    iPAddress = GetIpFromHost(addressAndPort[0]);
+                }
+                if (!int.TryParse(addressAndPort[1], out port))
+                {
+                    Logger.Error("Invalid port: {0}", address);
+                    return false;
+                }
+                IPEndPoint endPoint = new IPEndPoint(iPAddress, port);
+
+                Logger.Debug("Connecting to dserver: {0}", endPoint);
+                socket = new Socket(iPAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                socket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.NoDelay, true);
+                socket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReceiveTimeout, 500);
+                socket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.KeepAlive, true);
+                socket.Connect(endPoint);
+                Logger.Debug("Connected ok");
+
+                socketAddress = address;
+                rconUser = user;
+                rconPassword = password;
+
+                string response = SendCommand(string.Format("AUTH {0} {1}", rconUser, rconPassword));
+                if (GetResponseCode(response) != 1)
+                {
+                    Logger.Warn("Could not authorize with dserver.");
+                    ShutdownSocket();
+                    return false;
+                }
+                return true;
+            }
+            catch (FormatException ex)
+            {
+                Logger.Error("Could not parse host address and/or port '{0}': {1}", address, ex.Message);
+                return false;
+            }
+            catch (SocketException ex)
+            {
+                Logger.Warn("Failed to connect to dserver {0}: {1}", address, ex.Message);
+                return false;
+            }
+        }
+
+        private static readonly Regex RE_STATUS = new Regex(@"STATUS=(?<status>\d+)");
+
+        private int GetResponseCode(string response)
+        {
+            if (response == null || string.Empty.Equals(response))
+            {
+                return -1;
+            }
+            Match m = RE_STATUS.Match(response);
+            if (m.Success)
+            {
+                if (int.TryParse(m.Groups["status"].Value, out int status))
+                {
+                    return status;
+                }
+            }
+            Logger.Warn("Could not determine response code from response: {0}", response);
+            return -1;
+        }
+
+        private static readonly Regex RE_PLAYER_LIST = new Regex(@"playerList=(?<player_list>.+)$");
+        private List<PlayerInfo> DecodePlayerList(string playerListResponse)
+        {
+            List<PlayerInfo> result = new List<PlayerInfo>();
+            Match m = RE_PLAYER_LIST.Match(playerListResponse);
+            if (m.Success)
+            {
+                string decoded = HttpUtility.UrlDecode(m.Groups["player_list"].Value);
+                foreach (var csvInfo in decoded.Split('|'))
+                {
+                    string[] fields = csvInfo.Split(',');
+                    if (fields.Length != 6)
+                    {
+                        Logger.Warn("Unable to parse playerList response fragment: {0}", csvInfo);
+                        continue;
+                    }
+                    if ("cId".Equals(fields[0]))
+                    {
+                        // This is the header row
+                        continue;
+                    }
+                    int cid = int.Parse(fields[0]);
+                    int status = int.Parse(fields[1]);
+                    int ping = int.Parse(fields[2]);
+                    string name = HttpUtility.UrlDecode(fields[3]);
+                    string playerIdGuid = HttpUtility.UrlDecode(fields[4]);
+                    string profileId = HttpUtility.UrlDecode(fields[5]);
+                    // TODO: Maybe we need things like client ID in future, for now just name and status should do
+                    result.Add(new PlayerInfo(name, status));
+                }
+            }
+            return result;
+        }
+
+
+        private string SendCommand(string command)
+        {
+            // Commands consist of an ASCII null-terminated string, preceded by a short
+            // which tells the length of the following command.
+
+            Logger.Debug("Sending command string: {0}", command);
+
+            byte[] commandBytes = Encoding.ASCII.GetBytes(command);
+
+            // Figure out the length including null terminator
+            short length = Convert.ToInt16(commandBytes.Length + 1);
+            byte[] lengthBytes = BitConverter.GetBytes(length);
+
+            byte[] assembled = new byte[lengthBytes.Length + commandBytes.Length + 1];
+            lengthBytes.CopyTo(assembled, 0);
+            commandBytes.CopyTo(assembled, 2);
+            assembled[assembled.Length - 1] = 0x00;
+
+            socket.Send(assembled);
+
+            byte[] receiveBuffer = new byte[16384];
+            int bytesReceived = socket.Receive(receiveBuffer);
+            Logger.Trace("Received {0} bytes from server", bytesReceived);
+
+            // Response includes a null terminating byte, ignore that when converting to ASCII
+            string response = Encoding.ASCII.GetString(receiveBuffer, 0, bytesReceived - 1);
+            Logger.Trace("Decoded to ASCII: {0}", response);
+            return response;
+        }
+
+        private IPAddress GetIpFromHost(string host)
+        {
+            var hosts = Dns.GetHostAddresses(host);
+            if (hosts == null || hosts.Length == 0)
+            {
+                throw new FormatException(string.Format("Host not found: {0}", host));
+            }
+            return hosts[0];
+        }
+
+        private class PlayerInfo
+        {
+            public PlayerInfo(string name, int status)
+            {
+                Name = name;
+                Status = status;
+            }
+
+            public string Name { get; }
+            public int Status { get; }
+        }
+    }
+}
---
+++ IL2-SimpleRadio Server/Network/Il2LogMonitor.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using NLog;
+using System.Threading;
+
+/*
+    Monitor an IL2 dserver logs directory. In the server startup.cfg, set
+      mission_text_log = 1
+      text_log_folder = "logs\text\"
+    to save text logs. Then point this code at the output directory.
+
+    Every time a new mission starts, a new 'zero' log is emitted. We reset
+    our knowledge of players coalitions at that point. When a player spawns
+    a plane, we remember their coalition.
+
+    We don't track players quitting or being killed, thus they never go back
+    to the Spectator coalition. This might be ok - being a spectator ought
+    to be benign.
+*/
+
+public class Il2LogMonitor
+{
+    private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+    private enum Coalition
+    {
+        Spectator,
+        Allies,
+        Axis,
+    }
+
+    private readonly string directory;
+    private readonly Dictionary<string, Coalition> playerCoalitions;
+    private bool keepRunning = true;
+
+    public Il2LogMonitor(string directory)
+    {
+        this.directory = directory;
+        this.playerCoalitions = new Dictionary<string, Coalition>();
+    }
+
+    // TODO: Correct way to spin up a thread and stop later?
+    public void Start()
+    {
+        Logger.Info("Catching up on latest log files...");
+        // Find the newest 'zero' log, and process that and any more 
+        // recent log files. This brings us up to date with a currently
+        // running mission (if any).
+        foreach (string logfile in GetNewestZeroLogAndSubsequentFiles())
+        {
+            ProcessLog(logfile);
+        }
+
+        // Now use the filesystem to watch for new logs being created.
+        Logger.Info("Monitoring for new log files...");
+        using (FileSystemWatcher watcher = new FileSystemWatcher())
+        {
+            watcher.Path = directory;
+            watcher.Filter = "missionReport*.txt";
+            watcher.Created += OnNewLogFile;
+
+            watcher.EnableRaisingEvents = true;
+
+            while(keepRunning)
+            {
+                Thread.Sleep(500);
+            }
+        }
+    }
+
+    private void OnNewLogFile(object source, FileSystemEventArgs e) =>
+        ProcessLog(e.FullPath);
+
+    private List<string> GetNewestZeroLogAndSubsequentFiles()
+    {
+        string[] allLogs = Directory.GetFiles(directory, "missionReport*.txt");
+        Array.Sort(allLogs, new LogComparer());
+        int mostRecentZeroLog = Int32.MaxValue;
+        for (int i = allLogs.Length - 1; i >= 0; i--)
+        {
+            if (allLogs[i].Contains("[0].txt"))
+            {
+                mostRecentZeroLog = i;
+                Console.WriteLine("Most recent zero log: " + allLogs[i]);
+                break;
+            }
+        }
+        List<string> result = new List<string>();
+        for (int i = mostRecentZeroLog; i < allLogs.Length; i++)
+        {
+            result.Add(allLogs[i]);
+        }
+        return result;
+    }
+
+    private void ProcessLog(string logfile)
+    {
+        Console.WriteLine("Processing " + logfile);
+
+        if (logfile.Contains("[0].txt"))
+        {
+            // 'Zero' log, so the map has rolled. Everything we know about coalitions is defunct.
+            playerCoalitions.Clear();
+        }
+        using (StreamReader sr = new StreamReader(logfile))
+        {
+            string line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                ProcessLogLine(line);
+            }
+            sr.Close();
+        }
+    }
+
+    private readonly Regex RE_SPAWN = new Regex(
+        @"^T:(?<tik>\d+) AType:10 PLID:(?<aircraft_id>\d+) PID:(?<bot_id>\d+) BUL:(?<cartridges>\d+) SH:(?<shells>\d+) BOMB:(?<bombs>\d+) RCT:(?<rockets>\d+) \((?<pos>.+)\) IDS:(?<profile_id>[-\w]{36}) LOGIN:(?<account_id>[-\w]{36}) NAME:(?<name>.*) TYPE:(?<aircraft_name>[\w\(\) .\-_/]+) COUNTRY:(?<country_id>\d{1,3}) FORM:(?<form>\d+) FIELD:(?<airfield_id>\d+) INAIR:(?<airstart>\d) PARENT:(?<parent_id>[-\d]+) ISPL:(?<is_player>\d+) ISTSTART:(?<is_tracking_stat>\d+) PAYLOAD:(?<payload_id>\d+) FUEL:(?<fuel>\S{5,6}) SKIN:(?<skin>[\S ]*) WM:(?<weapon_mods_id>\d+)",
+        RegexOptions.Compiled);
+
+    private void ProcessLogLine(string line)
+    {
+        Match m = RE_SPAWN.Match(line);
+        if (m.Success)
+        {
+            string name = m.Groups["name"].Value;
+            string country_id = m.Groups["country_id"].Value;
+            Coalition coalition = CoalitionFor(country_id);
+            playerCoalitions[name] = coalition;
+            Console.WriteLine("Set player {0} to coalition {1} ", name, coalition);
+        }
+    }
+
+    private Coalition CoalitionFor(string country_id)
+    {
+        if (country_id.StartsWith("1"))
+        {
+            return Coalition.Allies;
+        }
+        else if (country_id.StartsWith("2"))
+        {
+            return Coalition.Axis;
+        }
+        else
+        {
+            return Coalition.Spectator;
+        }
+    }
+
+    class LogComparer : IComparer<string>
+    {
+        // Log file names look like this: missionReport(2020-08-05_03-42-16)[52].txt
+        // Can't compare them alphabetically. Need to pull out the date/time component
+        // and the file sequence number and compare on those two.
+        private readonly Regex rx = new Regex(@"missionReport\((.+)\)\[(\d+)\]\.txt$", RegexOptions.Compiled);
+
+        public int Compare(string x, string y)
+        {
+            (string x_time, int x_num) = GetLogTimeAndNumberFromFilename(x);
+            (string y_time, int y_num) = GetLogTimeAndNumberFromFilename(y);
+
+            int compareByTime = x_time.CompareTo(y_time);
+            if (compareByTime != 0)
+            {
+                return compareByTime;
+            }
+            return x_num.CompareTo(y_num);
+        }
+
+        private (string, int) GetLogTimeAndNumberFromFilename(string fileName)
+        {
+            Match match = rx.Match(fileName);
+            if (match.Success)
+            {
+                string logTime = match.Groups[1].Value;
+                int logNum = Int32.Parse(match.Groups[2].Value);
+                return (logTime, logNum);
+            }
+            else
+            {
+                // TODO: Gross.
+                throw new Exception("Could not get log time and number from: " + fileName);
+            }
+        }
+    }
+}
